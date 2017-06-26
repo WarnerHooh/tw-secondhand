6ccdf54257fc29d41067ffc2bc7b5109422c16b7
Eric Zeng <leizeng@CNleizeng-2.local>
2017-06-27 00:13:49 +0800

    test cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('App shallow renders with initial store', () => {
+  const wrapper = shallow(<App />);
+  const store = wrapper.props().store.getState();
+  expect(store).toBeTruthy();
+  expect(store.app).toBeTruthy();
 });
