22212190a1c52640676e8114385a2118ef6c10da
Eric Zeng <leizeng@CNleizeng-2.local>
2017-06-26 21:14:01 +0800

    initial state

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,6 +5,7 @@ import { History } from 'history';
 import * as storage from 'redux-storage';
 import createEngine from 'redux-storage-engine-localstorage';
 
+import initialState from './initialState';
 import reducers from './reducers';
 import middlewares from './middlewares';
 
@@ -12,16 +13,18 @@ const storeConfigure = (history: History) => {
     const reducer = storage.reducer(combineReducers(reducers));
     const router = routerMiddleware(history);
 
+    // bump up storage version if state structure change
     const engine = createEngine('myApp-0.0.1');
     const storeMiddleware = storage.createMiddleware(engine, [], [
-    // White list actions, do not save anything if leave blank here
+    // White list actions, will not save anything if leave blank here
     ]);
 
     const createStoreWithMiddleware = applyMiddleware(router, storeMiddleware, ...middlewares)(createStore);
     
-    const store = createStoreWithMiddleware(reducer);
+    const store = createStoreWithMiddleware(reducer, initialState);
     
     const load = storage.createLoader(engine);
+    // will trigger storage load action
     load(store);
 
     return store;
