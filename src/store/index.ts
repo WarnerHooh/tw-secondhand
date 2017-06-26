08646e21f68d67382b775016a223e165c68bf7e8
Eric Zeng <leizeng@CNleizeng-2.local>
2017-06-26 21:07:52 +0800

    add storage in localstorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,16 +2,29 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import { History } from 'history';
 
+import * as storage from 'redux-storage';
+import createEngine from 'redux-storage-engine-localstorage';
+
 import reducers from './reducers';
 import middlewares from './middlewares';
 
 const storeConfigure = (history: History) => {
-    const reducer = combineReducers(reducers);
+    const reducer = storage.reducer(combineReducers(reducers));
     const router = routerMiddleware(history);
+
+    const engine = createEngine('myApp-0.0.1');
+    const storeMiddleware = storage.createMiddleware(engine, [], [
+    // White list actions, do not save anything if leave blank here
+    ]);
+
+    const createStoreWithMiddleware = applyMiddleware(router, storeMiddleware, ...middlewares)(createStore);
     
-    const createStoreWithMiddleware = applyMiddleware(router, ...middlewares)(createStore);
+    const store = createStoreWithMiddleware(reducer);
     
-    return createStoreWithMiddleware(reducer);
+    const load = storage.createLoader(engine);
+    load(store);
+
+    return store;
 };
 
 export default storeConfigure;
