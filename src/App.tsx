50ea2e5ca345e2906594e9c617e92b141416f478
Eric Zeng <leizeng@CNleizeng-2.local>
2017-06-26 20:53:03 +0800

    move store to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,22 @@
-// import * as React from 'react';
-import Root from './containers/Root';
+import * as React from 'react';
+import { Provider } from 'react-redux';
+import createHistory from 'history/createBrowserHistory';
+import { Route } from 'react-router';
+import { ConnectedRouter } from 'react-router-redux';
 
-export default Root;
+import storeConfigure from './store';
+import HomePage from './containers/pages/Home/HomePage';
+// import routers from '../store/routers';
+
+const history = createHistory();
+const store = storeConfigure(history);
+
+export default () => (
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <div>
+                <Route path="/" component={HomePage}/>
+            </div>
+        </ConnectedRouter>
+    </Provider>
+);
