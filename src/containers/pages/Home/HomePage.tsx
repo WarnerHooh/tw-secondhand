37c57f6e760f5e9e13287ae38b1dcb3578c50a16
Eric Zeng <leizeng@CNleizeng-2.local>
2017-06-28 17:40:06 +0800

    router samples

--- src/containers/pages/Home/HomePage.tsx
+++ src/containers/pages/Home/HomePage.tsx
@@ -1,15 +1,29 @@
 import * as React from 'react';
+import { connect, DispatchProp } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
+import { push } from 'react-router-redux';
+
 import * as Components from '../../../components';
 import './HomePage.css';
 
-export default () => (
-    <div className="App">
-        <div className="App-header">
-            <Components.Logo />
-            <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-            To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-    </div>
-);
+class HomePage extends React.Component<DispatchProp<object> & RouteComponentProps<object>> {
+    render() {
+        const { dispatch } = this.props;
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <Components.Logo />
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <p>
+                    <button onClick={() => dispatch(push('about-us'))}>Go to About Us</button>
+                </p>
+            </div>
+        );
+    }
+}
+
+export default connect()(HomePage);
--- src/store/Routers.tsx
+++ src/store/Routers.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import { Route } from 'react-router';
 
 import HomePage from '../containers/pages/Home/HomePage';
+import AboutUsPage from '../containers/pages/AboutUs/AboutUsPage';
 
 const router = (
     <div>
-        <Route path="/" component={HomePage}/>
+        <Route exact={true} path="/" component={HomePage}/>
+        <Route path="/about-us" component={AboutUsPage}/>
     </div>
 );
 
