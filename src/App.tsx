b7f091ecab5b569efbb24309dbf14c4197b2314b
Eric Zeng <leizeng@CNleizeng-2.local>
2017-06-28 20:31:23 +0800

    adjust the router file name

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import createHistory from 'history/createBrowserHistory';
 import { ConnectedRouter } from 'react-router-redux';
 
 import storeConfigure from './store';
-import Routers from './store/Routers';
+import routers from './store/routers';
 
 const history = createHistory();
 const store = storeConfigure(history);
 
 export default () => (
     <Provider store={store}>
-        <ConnectedRouter history={history}>{Routers}</ConnectedRouter>
+        <ConnectedRouter history={history}>{routers}</ConnectedRouter>
     </Provider>
 );
