d28d29a51c6475614d21d0fe83b1cad24bada247
Lingfang Li <lfli@CNlfli.local>
2017-07-28 17:13:51 +0800

    [Lingfang]fix conflict

---
+++ src/containers/pages/Login/LoginPage.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+
+import './LoginPage.css';
+import '../../../style/Common.css';
+
+const loginImg = require('../../../style/assets/login.png');
+
+interface LoginProps {
+
+}
+
+interface LoginState {
+      username: string;
+      password: string;
+}
+
+class LoginPage extends React.Component<LoginProps, LoginState> {
+    constructor(props: LoginProps) {
+           super(props);
+           this.state = {
+             username: '',
+             password: ''
+           };
+        }
+
+    public handleSubmit() {
+      alert('submit');
+    }
+
+    public handleChange(key, value) {
+      this.setState({[key]: value});
+    }
+
+    render() {
+      return (
+            <div className="login">
+                <div className="login-header">
+                    <span>请登录</span>
+                </div>
+                <div className="login-main">
+                  <img src= {loginImg} className="login-img" />
+                  <input type="text" className="input" placeholder="用户名" onChange={(e)=> this.handleChange('username', e.target.value)}/>
+                  <input type="text" className="input" placeholder="密码"  onChange={(e) => this.handleChange('password', e.target.value)}/>
+                  <button className="dis-btn" onClick={this.handleSubmit}>  登录 </button>
+                  <button className="btn">  免费注册 </button>
+                </div>
+            </div>
+       );
+    }
+}
+
+export default LoginPage;
--- src/store/routers.tsx
+++ src/store/routers.tsx
@@ -4,12 +4,14 @@ import { Route } from 'react-router';
 import HomePage from '../containers/pages/Home/HomePage';
 import SignUpPage from '../containers/pages/SignUp/SignUpPage';
 import ProfilePage from '../containers/pages/Profile/ProfilePage';
+import LoginPage from '../containers/pages/Login/LoginPage';
 
 const router = (
     <div>
         <Route exact={true} path="/" component={HomePage}/>
         <Route path="/sign-up" component={SignUpPage}/>
         <Route path="/profile" component={ProfilePage}/>
+        <Route path="/login" component={LoginPage} />
     </div>
 );
 
