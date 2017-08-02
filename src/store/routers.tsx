05f73a8cc77035d2646d053086c9e1dd425b2b39
Sasa33 <chengsasa33@gmail.com>
2017-08-02 13:09:38 +0800

    [Sasa] fix unneccessary routers and fix eslint issue

--- src/store/routers.tsx
+++ src/store/routers.tsx
@@ -2,18 +2,14 @@ import * as React from 'react';
 import { Route } from 'react-router';
 
 import HomePage from '../containers/pages/Home/HomePage';
-import SignUpPage from '../containers/pages/SignUp/SignUpPage';
 import ProfilePage from '../containers/pages/Profile/ProfilePage';
-import LoginPage from '../containers/pages/Login/LoginPage';
 import OwnedPage from '../containers/pages/Owned/OwnedPage';
 import BoughtPage from '../containers/pages/Bought/BoughtPage';
 
 const router = (
   <div>
     <Route exact={true} path="/" component={HomePage}/>
-    <Route path="/sign-up" component={SignUpPage}/>
     <Route path="/profile" component={ProfilePage}/>
-    <Route path="/login" component={LoginPage}/>
     <Route path="/owned" component={OwnedPage}/>
     <Route path="/bought" component={BoughtPage}/>
   </div>
