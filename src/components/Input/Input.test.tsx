ae90a3d1c48d475007586fba265e84fef0279c35
FangXie <fxie@CNfangxie.local>
2017-08-07 16:58:52 +0800

    [XieFang]: fix input test

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -3,37 +3,37 @@ import { render, shallow } from 'enzyme';
 import { spy } from 'sinon';
 import Input from './Input';
 
-it('List shallow renders with id', () => {
+it('Input shallow renders with id', () => {
   const wrapper = render(<Input id="input-id"/>);
 
   expect(wrapper.find('#input-id').length).toBe(1);
 });
 
-it('List shallow renders with type', () => {
+it('Input shallow renders with type', () => {
   const wrapper = render(<Input type="file"/>);
 
   expect(wrapper.find('[type="file"]').length).toBe(1);
 });
 
-it('List shallow renders with placeholder', () => {
+it('Input shallow renders with placeholder', () => {
   const wrapper = render(<Input placeholder="input your name"/>);
 
   expect(wrapper.find('[placeholder="input your name"]').length).toBe(1);
 });
 
-it('List shallow renders with className', () => {
+it('Input shallow renders with className', () => {
   const wrapper = render(<Input className="input-class"/>);
 
   expect(wrapper.find('.input-class').length).toBe(1);
 });
 
-it('List shallow renders with accept', () => {
+it('Input shallow renders with accept', () => {
   const wrapper = render(<Input accept="image/*"/>);
 
   expect(wrapper.find('[accept="image/*"]').length).toBe(1);
 });
 
-it('List shallow renders with onChange', () => {
+it('Input shallow renders with onChange', () => {
   const onChange = spy();
   const wrapper = shallow(<Input onChange={onChange}/>);
   const inputWrapper = wrapper.find('input');
@@ -41,7 +41,7 @@ it('List shallow renders with onChange', () => {
   expect(onChange.called).toBe(true);
 });
 
-it('List shallow renders with id, type, placeholder, className, accept, onChange', () => {
+it('Input shallow renders with id, type, placeholder, className, accept, onChange', () => {
   const onChange = spy();
   const wrapper = shallow(<Input id="input-id" type="file" placeholder="input your name" className="input-class" accept="image/*" onChange={onChange}/>);
 
